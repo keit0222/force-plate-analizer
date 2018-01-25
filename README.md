d5dea710736fdc74815914d5f1e7a73fc1c3683c
Keita Tomochika <tomochika.keita.tz1@is.naist.jp>
2018-01-25 21:41:14 +0900

    [change].gitignore is updated

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # force-plate-analizer
 A library to support the force palate analysis.
+
--- openForce/trans_test.py
+++
@@ -1,9 +0,0 @@
-
-# coding: utf-8
-
-from coordinate_transform import *
-import numpy as np
-
-vec = [-4.,-3.,-1.]
-mat = get_rotation_matrix_from_quaternion(get_quaternion(vec, np.pi/5))
-get_rotation_info(mat)
